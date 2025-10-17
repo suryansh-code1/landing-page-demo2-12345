fe788c1e3ad6cca24a9232dbddc58f110b9ded83
suryansh-code1 <suryanshbajpai111@gmail.com>
2025-10-17 09:11:45 +0530

    Add script.js from LLM

---
+++ script.js
@@ -0,0 +1,6 @@
+document.getElementById('loginForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    const username = document.getElementById('username').value;
+    const password = document.getElementById('password').value;
+    alert(`Username: ${username}\nPassword: ${password}`);
+});
